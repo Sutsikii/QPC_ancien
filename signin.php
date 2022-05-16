4352562cb8b8e9e5773da565597300d043928845
Sutsiki <nicolas-dutertre@bbox.fr>
2022-05-16 15:32:27 +0200

    Page d'inscription & some fixes

---
+++ signin.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/signin.css">
+    <title>Créer son compte</title>
+</head>
+<body>
+<div class="page-root">
+        <div class="login-form">
+            <h1>Question pour un café ! </h1>
+            <form action="" method="POST">
+                <div class="form-name">
+                    <div class="form-label">
+                        <label for="name"> Prénom </label>
+                    </div>
+                    <input type="name" name="name">
+                </div>
+                <div class="form-email">
+                    <div class="form-label">
+                        <label for="Email"> Email</label>
+                    </div>
+                    <input type="email" name="Email">
+                </div>
+                <div class="form-password">
+                <div class="form-label">
+                    <label for="password"> Mot de passe</label>
+                </div>
+                    <input type="text" name="password">
+                </div>
+                <div class="form-password">
+                <div class="form-label">
+                    <label for="repeat-password"> Confirmer Mot de passe</label>
+                </div>
+                    <input type="text" name="repeat-password">
+                </div>
+                <div class="form-button">
+                <button class="btn"> Se connecter </button>
+                </div>
+            </form>
+        </div>
+        <div class="footer-link">
+            <div class="footer-root">
+                <div class="span-af-form">
+                    <span><a href="#">© Question Pour un café </a></span>
+                    <span><a href="#">Contact</a></span>
+                    <span><a href="#">RGPD</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
